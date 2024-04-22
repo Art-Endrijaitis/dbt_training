0b4c1ff0fb612687eea444bbec6fc239f14c849a
Arthur Endrijaitis <art.endrijaitis@gmail.com>
2024-04-22 21:03:00 +0000

    Developed RAW_ORDERS, RAW_CUSTOMER and RAW_PRODUCT.

---
+++ models/RAW/RAW_CUSTOMER.sql
@@ -0,0 +1,11 @@
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+
+SELECT *
+FROM RAW.GLOBALMART.CUSTOMER
---
+++ models/RAW/RAW_ORDERS.sql
@@ -0,0 +1,11 @@
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+
+SELECT *
+FROM RAW.GLOBALMART.ORDERS
